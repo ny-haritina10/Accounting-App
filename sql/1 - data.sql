b3b025d574cd166d8ff76eca27acadad46d45d9b
Ny Haritina <142341722+ny-haritina10@users.noreply.github.com>
2025-05-23 19:54:00 +0300

    [backend] init spring boot project

--- sql/1 - data.sql
+++ sql/1 - data.sql
@@ -5,3 +5,10 @@ INSERT INTO roles (label, role_value) VALUES ('CEO', 1);
 INSERT INTO roles (label, role_value) VALUES ('HEAD OF DEPARTEMENT', 2);
 INSERT INTO roles (label, role_value) VALUES ('MANAGER', 3);
 INSERT INTO roles (label, role_value) VALUES ('ACCOUNTANT', 4);
+
+--
+--
+--
+
+INSERT INTO users (user_name, user_password) VALUES ('admin', crypt('admin', gen_salt('bf')));
+INSERT INTO user_roles (id_user, id_role) VALUES (2, 1); -- admin is user id 2
